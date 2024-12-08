6ccbd435faccbe6dfa0abf622df919283ae47d56
Tarun Boddupalli <tarunbod@gmail.com>
2024-12-08 10:53:16 -0500

    Refactor day 8

--- 2024/day8.rb
+++ 2024/day8.rb
@@ -1,58 +1,45 @@
-grid = STDIN.read.split("\n").map(&:chars)
-size = grid.size
-freqs = Hash.new { |h, k| h[k] = [] }
-grid.each_with_index do |row, y|
-  row.each_with_index do |c, x|
-    if c != '.'
-      freqs[c] << [y, x]
+def get_antinode_pos(a1, a2, n)
+  [a1[0] + n * (a1[0] - a2[0]), a1[1] + n * (a1[1] - a2[1])]
+end
+
+def in_bounds(pos, size)
+  pos[0] >= 0 && pos[0] < size && pos[1] >= 0 && pos[1] < size
+end
+
+def get_antinodes_in_bounds(a1, a2, size, maxn)
+  n = 1
+  antinodes = Set.new
+  while n <= maxn do
+    antinode = get_antinode_pos(a1, a2, n)
+    break unless in_bounds(antinode, size)
+    antinodes << antinode
+    n += 1
+  end
+  antinodes
+end
+
+def count_antinodes(grid, part2)
+  size = grid.size
+  freqs = Hash.new { |h, k| h[k] = [] }
+  grid.each_with_index do |row, y|
+    row.each_with_index do |c, x|
+      freqs[c] << [y, x] if c != '.'
     end
   end
-end
 
-def in_bounds(n1, size)
-  n1[0] >= 0 && n1[0] < size && n1[1] >= 0 && n1[1] < size
-end
-
-def count_antinodes(freqs, size, part2)
-  count = 0
   antinodes = Set.new
   freqs.each do |f, antenna|
     antenna.combination(2).each do |a1, a2|
-      if part2
-        antinodes << a1
-        antinodes << a2
-      end
-
-      n = 0
-      while true do
-        n += 1
-        n1 = [a1[0] + n * (a1[0] - a2[0]), a1[1] + n * (a1[1] - a2[1])]
-        if !in_bounds(n1, size)
-          break
-        end
-        antinodes << n1
-        if !part2
-          break
-        end
-      end
-
-      n = 0
-      while true do
-        n += 1
-        n2 = [a2[0] + n * (a2[0] - a1[0]), a2[1] + n * (a2[1] - a1[1])]
-        if !in_bounds(n2, size)
-          break
-        end
-        antinodes << n2
-        if !part2
-          break
-        end
-      end
+      maxn = part2 ? 1000 : 1
+      antinodes += [a1, a2] if part2
+      antinodes += get_antinodes_in_bounds(a1, a2, size, maxn)
+      antinodes += get_antinodes_in_bounds(a2, a1, size, maxn)
     end
   end
 
-  puts antinodes.size + count
+  antinodes.size
 end
 
-count_antinodes(freqs, size, false)
-count_antinodes(freqs, size, true)
+grid = STDIN.read.split("\n").map(&:chars)
+puts count_antinodes(grid, false)
+puts count_antinodes(grid, true)
