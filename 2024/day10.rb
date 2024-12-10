ee63d8486c6ddbb76517ec9c55f8d015aa6dd975
Tarun Boddupalli <tarunbod@gmail.com>
2024-12-10 01:00:58 -0500

    refactor

--- 2024/day10.rb
+++ 2024/day10.rb
@@ -42,24 +42,24 @@ def bfs(grid, start)
   [results, parents]
 end
 
+def count_paths_to_0(grid, parents, root)
+  p = parents[root]
+  if p.size == 1 && grid[p[0][0]][p[0][1]] == 0
+    1
+  else
+    p.map { |root_parent| count_paths_to_0(grid, parents, root_parent) }.sum
+  end
+end
+
 def part1(grid, starts)
-  puts starts.map { |s| bfs(grid, s)[0].size }.sum
+  starts.map { |s| bfs(grid, s)[0].size }.sum
 end
 
 def part2(grid, starts)
-  all = starts.map do |s|
+  starts.map do |s|
     results, parents = bfs(grid, s)
-    def count_paths_to_0(grid, parents, root)
-      p = parents[root]
-      if p.size == 1 && grid[p[0][0]][p[0][1]] == 0
-        1
-      else
-        p.map { |root_parent| count_paths_to_0(grid, parents, root_parent) }.sum
-      end
-    end
     results.map { |r| count_paths_to_0(grid, parents, r) }.sum
-  end
-  puts all.sum
+  end.sum
 end
 
 grid = STDIN.readlines.map(&:strip).map(&:chars).map { |l| l.map { |c| c == '.' ? -1 : c.to_i } }
@@ -69,5 +69,5 @@ for y in 0...grid.length
     starts << [y,x] if grid[y][x] == 0
   end
 end
-part1(grid, starts)
-part2(grid, starts)
+puts part1(grid, starts)
+puts part2(grid, starts)
